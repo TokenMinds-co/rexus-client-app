c97a3e2ee825e0c772cf20b13fa17d520c92396d
Kevin Tanuwijaya <kevintanuwijayasao@gmail.com>
2024-10-24 15:52:43 +0700

    style: adjust footer

--- components/layouts/footer.tsx
+++ components/layouts/footer.tsx
@@ -3,11 +3,11 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="flex container mx-auto">
-      <div className="flex flex-col space-y-2 lg:flex-row lg:space-y-0 items-center justify-between p-5 bg-primary w-full h-full">
+    <div className="flex bg-primary w-full">
+      <div className="flex container mx-auto flex-col space-y-2 lg:flex-row lg:space-y-0 items-center justify-between p-5 w-full h-full">
         <Link
           href="/"
-          className="text-3xl font-orbitron font-semibold uppercase bg-gradient-to-b from-black to-primary text-transparent bg-clip-text"
+          className="text-3xl font-orbitron font-semibold uppercase bg-gradient-to-b from-black via-black to-primary text-transparent bg-clip-text"
         >
           Rexus
         </Link>
